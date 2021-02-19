ce9cce31d9f748656a4bd04fd7e5446fc32742d8
Alexandre Repaire-Carlier <repaire.alexandre@gmail.com>
2021-02-19 14:45:33 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Pokemon } from './pokemon';
+import { POKEMONS } from './mock-pokemons';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  //styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'my-app';
+export class AppComponent implements OnInit {
+  public title: String = "Liste des pokémons";
+  public pokemons: Pokemon[];
+
+  
+  ngOnInit() {
+    this.pokemons = POKEMONS;
+  }
+
+
+  selectPokemon(pokemon:Pokemon){
+    alert("Vous avez cliqué sur " + pokemon.name);
+  }
 }
